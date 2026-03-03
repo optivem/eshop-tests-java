7d402ed9caf21f13516f09e88e9d3a9063314da8
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-03-03 11:35:09 +0100

    feat: Rename GivenReturnsTime to GivenClock and update method signatures for consistency

--- dsl-port/src/main/java/com/optivem/eshop/systemtest/dsl/port/clock/given/Given.java
+++ dsl-port/src/main/java/com/optivem/eshop/systemtest/dsl/port/clock/given/Given.java
@@ -1,10 +1,10 @@
 package com.optivem.eshop.systemtest.dsl.port.clock.given;
 
-import com.optivem.eshop.systemtest.dsl.port.clock.given.steps.GivenReturnsTime;
+import com.optivem.eshop.systemtest.dsl.port.clock.given.steps.GivenClock;
 import com.optivem.eshop.systemtest.dsl.port.clock.then.Then;
 
 public interface Given {
-    GivenReturnsTime returnsTime();
+    GivenClock givenClock();
 
     Then then();
 }
--- dsl-port/src/main/java/com/optivem/eshop/systemtest/dsl/port/clock/given/steps/GivenClock.java
+++ dsl-port/src/main/java/com/optivem/eshop/systemtest/dsl/port/clock/given/steps/GivenClock.java
@@ -2,6 +2,6 @@ package com.optivem.eshop.systemtest.dsl.port.clock.given.steps;
 
 import com.optivem.eshop.systemtest.dsl.port.clock.given.steps.base.GivenStep;
 
-public interface GivenReturnsTime extends GivenStep {
-    GivenReturnsTime time(String time);
+public interface GivenClock extends GivenStep {
+    GivenClock time(String time);
 }
