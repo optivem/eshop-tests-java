75549076ed2f89957c2c3a760f0f0b46eb9e362e
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-03-05 11:28:53 +0100

    Add end-to-end and smoke tests for order viewing and system health checks
    - Implemented ViewOrderNegativeTest to verify error handling for non-existent orders.
    - Created ViewOrderPositiveTest to validate successful retrieval of placed orders.
    - Established BaseE2eTest class for shared setup in end-to-end tests.
    - Added PlaceOrderPositiveTest to confirm order placement functionality.
    - Developed smoke tests for ERP, Tax, Shop API, and Shop UI across multiple versions (v1 to v7) to ensure system health.
    - Introduced ClockSmokeTest in v7 to check clock service availability.
    - Updated BaseE2eTest classes for v6 and v7 to align with new testing structure.
    - Ensured all tests utilize appropriate channel types for UI and API interactions.

--- Run-SystemTests.Config.ps1
+++ Run-SystemTests.Config.ps1
@@ -14,78 +14,78 @@ $Config = @{
         # Smoke Tests
         @{  Id = "smoke-stub";
             Name = "Smoke Tests - Stubbed External Systems";
-            Command = "& .\gradlew.bat :system-test:smoke-test:test -Denvironment=local -DexternalSystemMode=stub";
+            Command = "& .\gradlew.bat :system-test:test -Dtype=smoke -DexternalSystemMode=stub -Denvironment=local";
             Path = ".";
-            TestReportPath = "system-test\smoke-test\build\reports\tests\test\index.html"
+            TestReportPath = "system-test\build\reports\tests\test\index.html"
             TestInstallCommands = $null; },
         @{  Id = "smoke-real";
             Name = "Smoke Tests - Real External Systems";
-            Command = "& .\gradlew.bat :system-test:smoke-test:test -Denvironment=local -DexternalSystemMode=real";
+            Command = "& .\gradlew.bat :system-test:test -Dtype=smoke -DexternalSystemMode=real -Denvironment=local";
             Path = ".";
-            TestReportPath = "system-test\smoke-test\build\reports\tests\test\index.html"
+            TestReportPath = "system-test\build\reports\tests\test\index.html"
             TestInstallCommands = $null; },
 
         # Acceptance Tests
         @{  Id = "acceptance-api";
             Name = "Acceptance Tests - Channel: API";
-            Command = "& .\gradlew.bat :system-test:acceptance-test:test -Denvironment=local -DexcludeTags=isolated -Dchannel=API";
+            Command = "& .\gradlew.bat :system-test:test -Dtype=acceptance -DexcludeTags=isolated -Dchannel=API -Denvironment=local";
             Path = ".";
-            TestReportPath = "system-test\acceptance-test\build\reports\tests\test\index.html"
+            TestReportPath = "system-test\build\reports\tests\test\index.html"
             TestInstallCommands = $null; },
         @{  Id = "acceptance-ui";
             Name = "Acceptance Tests - Channel: UI";
-            Command = "& .\gradlew.bat :system-test:acceptance-test:test -Denvironment=local -DexcludeTags=isolated -Dchannel=UI";
+            Command = "& .\gradlew.bat :system-test:test -Dtype=acceptance -DexcludeTags=isolated -Dchannel=UI -Denvironment=local";
             Path = ".";
-            TestReportPath = "system-test\acceptance-test\build\reports\tests\test\index.html"
+            TestReportPath = "system-test\build\reports\tests\test\index.html"
             TestInstallCommands = $null; },
         @{  Id = "acceptance-isolated-api";
             Name = "Acceptance Tests - Isolated - Channel: API";
-            Command = "& .\gradlew.bat :system-test:acceptance-test:test -Denvironment=local -DincludeTags=isolated -Dchannel=API";
+            Command = "& .\gradlew.bat :system-test:test -Dtype=acceptance -DincludeTags=isolated -Dchannel=API -Denvironment=local";
             Path = ".";
-            TestReportPath = "system-test\acceptance-test\build\reports\tests\test\index.html"
+            TestReportPath = "system-test\build\reports\tests\test\index.html"
             TestInstallCommands = $null; },
         @{  Id = "acceptance-isolated-ui";
             Name = "Acceptance Tests - Isolated - Channel: UI";
-            Command = "& .\gradlew.bat :system-test:acceptance-test:test -Denvironment=local -DincludeTags=isolated -Dchannel=UI";
+            Command = "& .\gradlew.bat :system-test:test -Dtype=acceptance -DincludeTags=isolated -Dchannel=UI -Denvironment=local";
             Path = ".";
-            TestReportPath = "system-test\acceptance-test\build\reports\tests\test\index.html"
+            TestReportPath = "system-test\build\reports\tests\test\index.html"
             TestInstallCommands = $null; },
 
         # External System Contract Tests
         @{  Id = "contract-stub";
             Name = "Contract Tests - Stubbed External Systems";
-            Command = "& .\gradlew.bat :system-test:external-system-contract-test:test -Denvironment=local -DexternalSystemMode=stub --tests `"*Stub*`"";
+            Command = "& .\gradlew.bat :system-test:test -Dtype=contract -DexternalSystemMode=stub -Denvironment=local --tests `"*Stub*`"";
             Path = ".";
-            TestReportPath = "system-test\external-system-contract-test\build\reports\tests\test\index.html"
+            TestReportPath = "system-test\build\reports\tests\test\index.html"
             TestInstallCommands = $null; },
         @{  Id = "contract-real";
             Name = "Contract Tests - Real External Systems";
-            Command = "& .\gradlew.bat :system-test:external-system-contract-test:test -Denvironment=local -DexternalSystemMode=real --tests `"*Real*`"";
+            Command = "& .\gradlew.bat :system-test:test -Dtype=contract -DexternalSystemMode=real -Denvironment=local --tests `"*Real*`"";
             Path = ".";
-            TestReportPath = "system-test\external-system-contract-test\build\reports\tests\test\index.html"
+            TestReportPath = "system-test\build\reports\tests\test\index.html"
             TestInstallCommands = $null; },
 
         # E2E Tests
         @{
             Id = "e2e-no-channel";
             Name = "E2E Tests - No Channel (v1, v2, v3)";
-            Command = "& .\gradlew.bat :system-test:e2e-test:test -Denvironment=local --tests `"com.optivem.eshop.systemtest.e2etests.v1.*`" --tests `"com.optivem.eshop.systemtest.e2etests.v2.*`" --tests `"com.optivem.eshop.systemtest.e2etests.v3.*`"";
+            Command = "& .\gradlew.bat :system-test:test -Denvironment=local --tests `"com.optivem.eshop.systemtest.e2etests.v1.*`" --tests `"com.optivem.eshop.systemtest.e2etests.v2.*`" --tests `"com.optivem.eshop.systemtest.e2etests.v3.*`"";
             Path = ".";
-            TestReportPath = "system-test\e2e-test\build\reports\tests\test\index.html";
+            TestReportPath = "system-test\build\reports\tests\test\index.html";
             TestInstallCommands = $null; },
         @{
             Id = "e2e-api";
             Name = "E2E Tests - Channel: API";
-            Command = "& .\gradlew.bat :system-test:e2e-test:test -Denvironment=local -Dchannel=API";
+            Command = "& .\gradlew.bat :system-test:test -Dtype=e2e -Dchannel=API -Denvironment=local";
             Path = ".";
-            TestReportPath = "system-test\e2e-test\build\reports\tests\test\index.html";
+            TestReportPath = "system-test\build\reports\tests\test\index.html";
             TestInstallCommands = $null; },
         @{
             Id = "e2e-ui";
             Name = "E2E Tests - Channel: UI";
-            Command = "& .\gradlew.bat :system-test:e2e-test:test -Denvironment=local -Dchannel=UI";
+            Command = "& .\gradlew.bat :system-test:test -Dtype=e2e -Dchannel=UI -Denvironment=local";
             Path = ".";
-            TestReportPath = "system-test\e2e-test\build\reports\tests\test\index.html";
+            TestReportPath = "system-test\build\reports\tests\test\index.html";
             TestInstallCommands = $null; }
 
     )
