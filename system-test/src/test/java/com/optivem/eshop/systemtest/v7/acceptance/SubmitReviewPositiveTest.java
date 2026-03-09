889a4952e7e6a6406c3d1ef30e9061615bd37783
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-03-09 20:19:33 +0100

    Submit Review | GREEN - System

--- system-test/src/test/java/com/optivem/eshop/systemtest/v7/acceptance/SubmitReviewPositiveTest.java
+++ system-test/src/test/java/com/optivem/eshop/systemtest/v7/acceptance/SubmitReviewPositiveTest.java
@@ -4,12 +4,10 @@ import com.optivem.eshop.systemtest.v7.acceptance.base.BaseAcceptanceTest;
 import com.optivem.eshop.systemtest.channel.ChannelType;
 import com.optivem.eshop.systemtest.driver.port.shop.dtos.OrderStatus;
 import com.optivem.testing.Channel;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.TestTemplate;
 
 class SubmitReviewPositiveTest extends BaseAcceptanceTest {
 
-    @Disabled("RED")
     @TestTemplate
     @Channel({ChannelType.UI, ChannelType.API})
     void canSubmitReviewOnDeliveredOrder() {
